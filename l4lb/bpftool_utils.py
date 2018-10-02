6c4a2c404ebdc24817084858c4bf78b49165773b
David Beckett <david.beckett@netronome.com>
2018-10-02 09:47:47 -0400

    L4LB: bpftool_utils.py - ensure utf-8 responses
    With Python3.5, check_output() responds with Bytes.
    Ensure it is decoded to utf-8 before parsing.
    
    Suggested-by: Quentin Monnet <quentin.monnet@netronome.com>
    Signed-off-by: David Beckett <david.beckett@netronome.com>
    Acked-by: Jakub Kicinski <jakub.kicinski@netronome.com>

--- l4lb/bpftool_utils.py
+++ l4lb/bpftool_utils.py
@@ -7,17 +7,20 @@ import subprocess
 import struct
 import sys
 
+def check_output_json(cmd):
+    return json.loads(subprocess.check_output(cmd, shell=True).decode("utf-8"))
+
 def get_xdp_prog(interface):
     cmd_iplink = 'ip -j link show %s' % interface
-    iplink = json.loads(subprocess.check_output(cmd_iplink, shell=True))
+    iplink = check_output_json(cmd_iplink)
     return iplink[0]['xdp']['prog']['id']
 
 def get_map_ids(interface):
     prog_id = get_xdp_prog(interface)
     cmd_progshow = 'bpftool prog show id %d -p' % prog_id
-    prog_info = json.loads(subprocess.check_output(cmd_progshow, shell=True))
+    prog_info = check_output_json(cmd_progshow)
 
-    maps = json.loads(subprocess.check_output('bpftool map -p', shell=True))
+    maps = check_output_json('bpftool map -p')
     map_ids = []
 
     for m in maps:
@@ -27,12 +30,12 @@ def get_map_ids(interface):
 
 def get_map_entries(map_id):
     cmd_mapshow = 'bpftool map show id %s -p' % map_id
-    map_info = json.loads(subprocess.check_output(cmd_mapshow, shell=True))
+    map_info = check_output_json(cmd_mapshow)
     return map_info['max_entries']
 
 def get_map_dev(map_id):
     cmd_mapshow = 'bpftool map show id %s -p' % map_id
-    map_info = json.loads(subprocess.check_output(cmd_mapshow, shell=True))
+    map_info = check_output_json(cmd_mapshow)
 
     if "dev" in map_info:
         return "Offload"
@@ -41,7 +44,7 @@ def get_map_dev(map_id):
 
 def dump_map(map_id):
     cmd_map = 'bpftool map dump id %s -p' % map_id
-    return json.loads(subprocess.check_output(cmd_map, shell=True))
+    return check_output_json(cmd_map)
 
 def hex_list_to_int(hex_list):
     hex_str = ''.join([byte.replace('0x', '') for byte in hex_list])
